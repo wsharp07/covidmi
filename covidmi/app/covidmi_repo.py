43c9cbcc4666e142d6bac90ccf46dadbb627c6c0
William Sharp <wsharp07@users.noreply.github.com>
2020-04-09 20:13:55 -0400

    Add support for pushing to ACR (#2)
    * Create build-image.yml
    
    * maybe this works?
    
    * switching off of orbs
    
    * adding flake8
    
    * fixing linting issues
    
    * fixing pytest
    
    * storing results
    
    * trying orbs again
    
    * trying again
    
    * maybe2
    
    * try again
    
    * swag 2
    
    * swag 3
    
    * fixing requirments
    
    * triggering build
    
    * removing path
    
    * run ls
    
    * renaming to Dockerfile
    
    * rename
    
    * generating push

--- covidmi/app/covidmi_repo.py
+++ covidmi/app/covidmi_repo.py
@@ -1,132 +1,131 @@
 from datetime import datetime
 import sqlite3
 
+
 class CovidmiRepo:
-  def __init__(self):
-    self.table_name = 'covidmi'
-    self.connection = sqlite3.connect('covid-mi.db')
-    self.cursor = self.connection.cursor()
-    self.today = datetime.utcnow().strftime('%Y-%m-%d')
 
-    self.create_table()
+    def __init__(self):
+        self.table_name = 'covidmi'
+        self.connection = sqlite3.connect('covid-mi.db')
+        self.cursor = self.connection.cursor()
+        self.today = datetime.utcnow().strftime('%Y-%m-%d')
 
-  def create_table(self):
-    create_sql = f"""
-    CREATE TABLE IF NOT EXISTS {self.table_name} (
-        Id INTEGER PRIMARY KEY,
-        County TEXT NOT NULL,
-        Cases INTEGER DEFAULT 0,
-        Deaths INTEGER DEFAULT 0,
-        CreatedAt TEXT
-    );"""
+        self.create_table()
 
-    self.cursor.execute(create_sql)
+    def create_table(self):
+        create_sql = f"""
+        CREATE TABLE IF NOT EXISTS {self.table_name} (
+            Id INTEGER PRIMARY KEY,
+            County TEXT NOT NULL,
+            Cases INTEGER DEFAULT 0,
+            Deaths INTEGER DEFAULT 0,
+            CreatedAt TEXT
+        );"""
 
-  def insert(self, data):
-    sql = f'''INSERT INTO {self.table_name} (County, Cases, Deaths, CreatedAt) 
-    VALUES (?,?,?,datetime('now'))'''
+        self.cursor.execute(create_sql)
 
-    self.cursor.execute(sql, data)
-    self.connection.commit()
+    def insert(self, data):
+        sql = f'''INSERT INTO {self.table_name} (County, Cases, Deaths, CreatedAt)
+        VALUES (?,?,?,datetime('now'))'''
 
-  def exists_for_today(self, county):
+        self.cursor.execute(sql, data)
+        self.connection.commit()
 
-    sql = f"""
-    SELECT COUNT(*) 
-    FROM {self.table_name}
-    WHERE 
-      {self.table_name}.County = '{county}'
-      AND date({self.table_name}.CreatedAt, 'localtime') = '{self.today}'
-    """
+    def exists_for_today(self, county):
 
-    self.cursor.execute(sql)
-    r = self.cursor.fetchone()
+        sql = f"""
+        SELECT COUNT(*)
+        FROM {self.table_name}
+        WHERE
+        {self.table_name}.County = '{county}'
+        AND date({self.table_name}.CreatedAt, 'localtime') = '{self.today}'
+        """
 
-    if (r[0] == 0): return False
-    return True
+        self.cursor.execute(sql)
+        r = self.cursor.fetchone()
 
-  def to_obj(self, db_result):
-    return {
-      'id': db_result[0],
-      'county': db_result[1],
-      'cases': db_result[2],
-      'deaths': db_result[3],
-      'createdAt': db_result[4]
-    }
+        if (r[0] == 0):
+            return False
+        return True
 
-  def to_obj_set(self, db_result_set):
-    obj_result = []
+    def to_obj(self, db_result):
+        return {
+            'id': db_result[0],
+            'county': db_result[1],
+            'cases': db_result[2],
+            'deaths': db_result[3],
+            'createdAt': db_result[4]
+        }
 
-    for result in db_result_set:
-      obj = self.to_obj(result)
-      obj_result.append(obj)
+    def to_obj_set(self, db_result_set):
+        obj_result = []
 
-    return obj_result
+        for result in db_result_set:
+            obj = self.to_obj(result)
+            obj_result.append(obj)
 
-  def get_all(self): 
-    sql = f"""
-    SELECT
-      {self.table_name}.Id,
-      {self.table_name}.County,
-      {self.table_name}.Cases,
-      {self.table_name}.Deaths,
-      datetime({self.table_name}.CreatedAt, 'localtime') AS [CreatedAt]
-    FROM
-      {self.table_name} 
-    ORDER BY
-      {self.table_name}.CreatedAt DESC"""
+        return obj_result
 
-    print(sql)
+    def get_all(self):
+        sql = f"""
+        SELECT
+            {self.table_name}.Id,
+            {self.table_name}.County,
+            {self.table_name}.Cases,
+            {self.table_name}.Deaths,
+            datetime({self.table_name}.CreatedAt, 'localtime') AS [CreatedAt]
+        FROM
+            {self.table_name}
+        ORDER BY
+            {self.table_name}.CreatedAt DESC"""
 
-    self.cursor.execute(sql)
-    result_set = self.cursor.fetchall()
+        print(sql)
 
-    return self.to_obj_set(result_set)
+        self.cursor.execute(sql)
+        result_set = self.cursor.fetchall()
 
-  def get_all_latest(self):
-    sql = f"""
-    SELECT
-      {self.table_name}.Id,
-      {self.table_name}.County,
-      {self.table_name}.Cases,
-      {self.table_name}.Deaths,
-      datetime({self.table_name}.CreatedAt, 'localtime') AS [CreatedAt]
-    FROM
-      {self.table_name}
-    WHERE
-      date({self.table_name}.CreatedAt, 'localtime') = '{self.today}'
-    ORDER BY
-      {self.table_name}.CreatedAt DESC"""
+        return self.to_obj_set(result_set)
 
-    print(sql)
+    def get_all_latest(self):
+        sql = f"""
+        SELECT
+            {self.table_name}.Id,
+            {self.table_name}.County,
+            {self.table_name}.Cases,
+            {self.table_name}.Deaths,
+            datetime({self.table_name}.CreatedAt, 'localtime') AS [CreatedAt]
+        FROM
+            {self.table_name}
+        WHERE
+            date({self.table_name}.CreatedAt, 'localtime') = '{self.today}'
+        ORDER BY
+            {self.table_name}.CreatedAt DESC"""
 
-    self.cursor.execute(sql)
-    result_set = self.cursor.fetchall()
+        print(sql)
 
-    return self.to_obj_set(result_set)
+        self.cursor.execute(sql)
+        result_set = self.cursor.fetchall()
 
-  def get_by_county(self, county):
-    sql = f"""
-    SELECT
-      {self.table_name}.Id,
-      {self.table_name}.County,
-      {self.table_name}.Cases,
-      {self.table_name}.Deaths,
-      datetime({self.table_name}.CreatedAt, 'localtime') AS [CreatedAt]
-    FROM
-      {self.table_name}
-    WHERE
-      {self.table_name}.County = '{county}' COLLATE NOCASE
-    ORDER BY
-      {self.table_name}.CreatedAt DESC"""
+        return self.to_obj_set(result_set)
 
-    print(sql)
+    def get_by_county(self, county):
+        sql = f"""
+        SELECT
+            {self.table_name}.Id,
+            {self.table_name}.County,
+            {self.table_name}.Cases,
+            {self.table_name}.Deaths,
+            datetime({self.table_name}.CreatedAt, 'localtime') AS [CreatedAt]
+        FROM
+            {self.table_name}
+        WHERE
+            {self.table_name}.County = '{county}' COLLATE NOCASE
+        ORDER BY
+            {self.table_name}.CreatedAt DESC"""
 
-    self.cursor.execute(sql)
-    result_set = self.cursor.fetchall()
+        print(sql)
 
-    return self.to_obj_set(result_set)
+        self.cursor.execute(sql)
+        result_set = self.cursor.fetchall()
 
-
-
-  
+        return self.to_obj_set(result_set)
--- covidmi/app/data_parser.py
+++ covidmi/app/data_parser.py
@@ -1,21 +1,27 @@
 class DataParser:
 
-  def is_valid(self, county):
-    if (county == 'Other*'): return False
-    if (county == 'Out of State'): return False
-    if (county == 'Unknown'): return False
-    if (county == 'Total'): return False
-    if (county == 'County'): return False
+    def is_valid(self, county):
+        if (county == 'Other*'):
+            return False
+        if (county == 'Out of State'):
+            return False
+        if (county == 'Unknown'):
+            return False
+        if (county == 'Total'):
+            return False
+        if (county == 'County'):
+            return False
 
-    return True
+        return True
 
-  def parse_county(self, table_data):
-    return table_data[0].text.strip()
+    def parse_county(self, table_data):
+        return table_data[0].text.strip()
 
-  def parse_deaths(self, table_data):
-    deaths = table_data[2].text.strip()
-    if (deaths == ''): return 0
-    return deaths
+    def parse_deaths(self, table_data):
+        deaths = table_data[2].text.strip()
+        if (deaths == ''):
+            return 0
+        return deaths
 
-  def parse_cases(self, table_data):
-    return table_data[1].text.strip()
+    def parse_cases(self, table_data):
+        return table_data[1].text.strip()
